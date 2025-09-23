5be9d57bc309b8701c105281b35e89ac3a33be2f
Ayushman Gupta <ayushman0021@gmail.com>
2025-09-23 20:08:48 +0530

    Refactor conditions.js: Comment out console logs and clean up code structure; Add loops.js with initial loop implementations

--- Section-1/javascript/conditions.js
+++ Section-1/javascript/conditions.js
@@ -4,11 +4,46 @@ if (a % 2 == 0) {
     let msg = "Even hi hai..!!";
     const b = 20
     var c = "This is var"
-    console.log("even")
+    // console.log("even")
 } else {
-    console.log("odd")
+    // console.log("odd")
 }
 
 // console.log(msg); // ReferenceError: msg is not defined
 // console.log(b);
-console.log(c); // var is function scoped or globally scoped
+// console.log(c); // var is function scoped or globally scoped
+
+// WAP to check if a user is able to vote or not.
+
+
+// WAP to print whether a number is divisible by 3 and 12.
+num1 = 66
+
+// if (num1 % 3 == 0 && num1 % 12 == 0) {
+//     console.log("Divisible");
+// } else {
+//     console.log("Not");
+// }
+
+if (num1 % 3 == 0 && num1 % 11 == 0) {
+    console.log("Divisible by Both");
+} else if (num1 % 3 == 0) {
+    console.log("Divisible by 3");
+} else if (num1 % 11 == 0) {
+    console.log("Divisible by 11");
+} else {
+    console.log("Not Divisible by Both");
+}
+
+// WAP to check if a number is a perfect sqr.
+
+let num2 = 49
+
+const root = num2 ** 0.5
+console.log(root)
+
+if (Number.isInteger(root)) {
+    console.log("Perfect Sqr - " + root)
+} else {
+    console.log("Not Perfect Sqr")
+}
---
+++ Section-1/javascript/loops.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 5; i++) {
+    // console.log(i)
+}
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 != 0) {
+        console.log(i)
+    }
+}
+
+// Wap to print numbers which are divisible by 12 and not by 3 between 1 to 100.
+
+// WAP to print all the numbers which are divisible by 3 and 5 and are also odd between 186 to 1523.
