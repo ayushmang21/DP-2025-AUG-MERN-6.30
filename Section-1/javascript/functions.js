74aa081a7838c5bbadc7e841560f3d375dada61f
Ayushman Gupta <ayushman0021@gmail.com>
2025-09-24 20:09:26 +0530

    Add loops.js with initial loop implementations and refactor functions.js to include basic arithmetic and average functions

---
+++ Section-1/javascript/functions.js
@@ -0,0 +1,19 @@
+// 1st Type Function
+function addNums(num1, num2) {
+    sum = num1 + num2
+    console.log(sum);
+}
+
+addNums(52, 75)
+addNums(52, 75)
+
+// 2nd Type Function
+
+const getAvg = function(m1,m2,m3){
+    avg = (m1 + m2 + m3)/3;
+    return avg;
+}
+
+const result = getAvg(52, 75, 89);
+
+console.log(result);
--- Section-1/javascript/loops.js
+++ Section-1/javascript/loops.js
@@ -4,10 +4,57 @@ for (let i = 0; i <= 5; i++) {
 
 for (let i = 1; i <= 50; i++) {
     if (i % 2 != 0) {
-        console.log(i)
+        // console.log(i)
     }
 }
 
 // Wap to print numbers which are divisible by 12 and not by 3 between 1 to 100.
 
 // WAP to print all the numbers which are divisible by 3 and 5 and are also odd between 186 to 1523.
+
+for (let i = 186; i <= 1523; i++) {
+    if (i % 3 == 0 && i % 5 == 0 && i % 2 != 0) {
+        // console.log(i)
+    }
+}
+
+// While loop
+
+let z = 10
+
+while (z <= 20) {
+    // console.log(z);
+    z++;
+    // console.log(z);
+    // console.log("------------------");
+}
+
+correct_pass = "admin123"
+
+pass = "admin123"
+
+while (pass == correct_pass) {
+    // console.log("Correct Password");
+    break;
+}
+
+let i = 0
+while (i <= 5) {
+    i++;
+    if (i == 3) {
+        continue;
+    }
+    // console.log(i);
+}
+
+// do while loop
+
+let b = 15
+do{
+    b++;
+    console.log(b);
+} while(b <= 15)
+
+
+// WAP to print all the numbers which are divisible by 7 and 11 between 1 to 1000.
+
