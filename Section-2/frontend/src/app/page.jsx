e2d5e70fe1868960b1ed9db9f867f16ae6cd657c
Ayushman Gupta <ayushman0021@gmail.com>
2025-10-27 20:32:07 +0530

    Refactor project structure: Reintroduce layout and page components; Add Navbar component; Update globals.css for Tailwind integration; Implement Home and Login pages.

---
+++ Section-2/frontend/src/app/page.jsx
@@ -0,0 +1,16 @@
+import Navbar from '@/components/Navbar';
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <a href="/login">Login</a>
+            <Navbar />
+        </>
+    )
+}
+
+export default Home;
