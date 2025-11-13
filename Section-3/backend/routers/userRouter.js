a292ca9df8bdd4a74f440cd7f856393bd869c427
Ayushman Gupta <ayushman0021@gmail.com>
2025-11-13 20:14:20 +0530

    Refactor user router to add delete and update endpoints, and improve error handling

--- Section-3/backend/routers/userRouter.js
+++ Section-3/backend/routers/userRouter.js
@@ -34,7 +34,36 @@ router.get('/getbyid/:id', (req, res) => {
             res.status(500).json(err);
             console.log(err);
         });
+});
 
+router.get('/getbycity/:city', (req, res) => {
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
+});
+
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
 })
 
 module.exports = router;
