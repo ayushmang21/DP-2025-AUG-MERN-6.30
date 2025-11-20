a3117c01fc5db3f2f5a60bcda78b784ae7370cf9
Ayushman Gupta <ayushman0021@gmail.com>
2025-11-20 20:04:55 +0530

    Enhance UpdateUser component layout with header and styled container

--- Section-2/frontend/src/app/update-user/[id]/page.jsx
+++ Section-2/frontend/src/app/update-user/[id]/page.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const UpdateUser = () => {
     return (
-        <div>UpdateUser</div>
+        <div>
+            <h1 className='text-3xl py-5 text-center font-bold'>Update User</h1>
+
+            <div className='container mx-auto bg-amber-300 p-10 rounded-lg shadow-lg'>
+                {
+
+                }
+            </div>
+
+        </div>
     )
 }
 
